c5170de10326c3444a0127f96a9f7d6d46c8977f
Sergantin <sharukan87@gmail.com>
2025-11-09 17:30:03 +0200

    New java

---
+++ test.js
@@ -0,0 +1,8 @@
+// Импортируем chalk
+import chalk from 'chalk';
+
+// Пример вывода цветного текста
+console.log(chalk.green('Привет, мир!'));
+console.log(chalk.blue.bold('Это пример с chalk!'));
+console.log(chalk.red.underline('Ошибка: что-то пошло не так'));
+console.log(chalk.yellow.bgBlack('Предупреждение: будь внимателен'));
